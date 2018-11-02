219f2fd8ec40bf3b118f4589e35852780886b07c
Jonathan Fischoff <jfischoff@yahoo.com>
2018-11-01 18:10:55 -0700

    updated readme, changelog, and deps

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # wait-on-port
+
+This is very simple utility library to block excution until a port is ready for connections.
+
+Here is an example of the primary function:
+
+```haskell
+import Network.Socket.Wait (wait)
+
+wait "127.0.0.1" 7000
+```
+
+This function will attempt to connect and delay 10 milliseconds and try again until it succeeds.
+
+In bash one could write:
+
+```bash
+while ! nc -z localhost 7000 ; do sleep 0.01 ; done
+```
+
+The above was copied from this stackoverflow answer https://stackoverflow.com/a/50008755
--- src/Network/Socket/Wait.hs
+++ src/Network/Socket/Wait.hs
@@ -37,26 +37,26 @@ wait = waitWith mempty 10000
 -- Advanced Api
 -------------------------------------------------------------------------------
 data EventHandlers = EventHandlers
-  { ehCreatedSocket :: IO ()
+  { createdSocket :: IO ()
   -- ^ Called after the socket is created
-  , ehDelaying      :: IO ()
+  , delaying      :: IO ()
   -- ^ Called after a failed attempt to connect before the thread
   -- is put to sleep.
-  , ehRestarting    :: IO ()
+  , restarting    :: IO ()
   -- ^ Called before a recursive call to restart the connection attempt
   }
 
 instance Monoid EventHandlers where
   mempty  = EventHandlers mempty mempty mempty
   mappend x y = EventHandlers = EventHandlers
-    { ehCreatedSocket = ehCreatedSocket x <> ehCreatedSocket y
-    , ehDelaying      = ehDelaying      x <> ehDelaying      y
-    , ehRestarting    = ehRestarting    x <> ehRestarting    y
+    { createdSocket = createdSocket x <> createdSocket y
+    , delaying      = delaying      x <> delaying      y
+    , restarting    = restarting    x <> restarting    y
     }
 
--- | Advanced usage. In most situations calling 'wait' will suffice. This allows one
--- to customize the delay between retries and debug the behavior of the function.
--- 'wait' is defined as
+-- | Advanced usage. In most situations calling 'wait' will suffice. This allows
+-- one to customize the delay between retries and debug the behavior of the
+-- function. 'wait' is defined as
 -- @
 --  wait = waitWith mempty 10000
 -- @
@@ -84,14 +84,14 @@ waitWith eh@EventHandlers {..} delay host port = do
       (S.socket (S.addrFamily addr) (S.addrSocketType addr) (S.addrProtocol addr))
       S.close
       $ \sock -> do
-        ehCreatedSocket
+        createdSocket
         S.connect sock $ S.addrAddress addr
 
   case res of
     Left (_ :: IOException) -> do
-      ehDelaying
+      delaying
       threadDelay delay
 
-      ehRestarting
+      restarting
       waitWith eh delay host port
     Right _ -> pure ()
